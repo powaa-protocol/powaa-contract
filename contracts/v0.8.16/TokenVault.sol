c050c570db6510a1239f2901764da172ccd19e35
KamenRiderPowaa <timothee.pratt@gmail.com>
2022-08-16 13:55:15 +0700

    refactor: fix linting

--- contracts/v0.8.16/TokenVault.sol
+++ contracts/v0.8.16/TokenVault.sol
@@ -10,204 +10,235 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "./interfaces/ITokenVault.sol";
 
-contract TokenVault is ITokenVault, ReentrancyGuardUpgradeable, PausableUpgradeable, OwnableUpgradeable {
-    using SafeMathUpgradeable for uint256;
-    using SafeERC20Upgradeable for IERC20Upgradeable;
+contract TokenVault is
+  ITokenVault,
+  ReentrancyGuardUpgradeable,
+  PausableUpgradeable,
+  OwnableUpgradeable
+{
+  using SafeMathUpgradeable for uint256;
+  using SafeERC20Upgradeable for IERC20Upgradeable;
 
-    /* ========== STATE VARIABLES ========== */
-    address public rewardsDistribution;
-    address public rewardsToken;
-    IERC20Upgradeable public stakingToken;
-    uint256 public periodFinish = 0;
-    uint256 public rewardRate = 0;
-    uint256 public rewardsDuration = 7 days;
-    uint256 public lastUpdateTime;
-    uint256 public rewardPerTokenStored;
+  /* ========== STATE VARIABLES ========== */
+  address public rewardsDistribution;
+  address public rewardsToken;
+  IERC20Upgradeable public stakingToken;
+  uint256 public periodFinish = 0;
+  uint256 public rewardRate = 0;
+  uint256 public rewardsDuration = 7 days;
+  uint256 public lastUpdateTime;
+  uint256 public rewardPerTokenStored;
 
-    mapping(address => uint256) public userRewardPerTokenPaid;
-    mapping(address => uint256) public rewards;
+  mapping(address => uint256) public userRewardPerTokenPaid;
+  mapping(address => uint256) public rewards;
 
-    uint256 private _totalSupply;
-    mapping(address => uint256) private _balances;
+  uint256 private _totalSupply;
+  mapping(address => uint256) private _balances;
 
-    /* ========== EVENTS ========== */
-    event RewardAdded(uint256 reward);
-    event Staked(address indexed user, uint256 amount);
-    event Withdrawn(address indexed user, uint256 amount);
-    event RewardPaid(address indexed user, uint256 reward);
-    event RewardsDurationUpdated(uint256 newDuration);
-    event Recovered(address token, uint256 amount);
+  /* ========== EVENTS ========== */
+  event RewardAdded(uint256 reward);
+  event Staked(address indexed user, uint256 amount);
+  event Withdrawn(address indexed user, uint256 amount);
+  event RewardPaid(address indexed user, uint256 reward);
+  event RewardsDurationUpdated(uint256 newDuration);
+  event Recovered(address token, uint256 amount);
 
-    /* ========== ERRORS ========== */
-    error TokenVault_CannotStakeZeroAmount();
-    error TokenVault_CannotWithdrawZeroAmount();
-    error TokenVault_ProvidedRewardTooHigh();
-    error TokenVault_CannotWithdrawStakingToken();
-    error TokenVault_RewardPeriodMustBeCompleted();
-    error TokenVault_NotRewardsDistributionContract();
+  /* ========== ERRORS ========== */
+  error TokenVault_CannotStakeZeroAmount();
+  error TokenVault_CannotWithdrawZeroAmount();
+  error TokenVault_ProvidedRewardTooHigh();
+  error TokenVault_CannotWithdrawStakingToken();
+  error TokenVault_RewardPeriodMustBeCompleted();
+  error TokenVault_NotRewardsDistributionContract();
 
-    /* ========== INITIALIZER ========== */
-    function initialize(
-        address _rewardsDistribution,
-        address _rewardsToken,
-        address _stakingToken)
-    external initializer {
-        ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
-        PausableUpgradeable.__Pausable_init();
-        OwnableUpgradeable.__Ownable_init();
+  /* ========== INITIALIZER ========== */
+  function initialize(
+    address _rewardsDistribution,
+    address _rewardsToken,
+    address _stakingToken
+  ) external initializer {
+    ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
+    PausableUpgradeable.__Pausable_init();
+    OwnableUpgradeable.__Ownable_init();
 
-        rewardsToken = _rewardsToken;
-        stakingToken = IERC20Upgradeable(_stakingToken);
-        rewardsDistribution = _rewardsDistribution;
+    rewardsToken = _rewardsToken;
+    stakingToken = IERC20Upgradeable(_stakingToken);
+    rewardsDistribution = _rewardsDistribution;
+  }
+
+  /* ========== VIEWS ========== */
+
+  function totalSupply() external view returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address account) external view returns (uint256) {
+    return _balances[account];
+  }
+
+  function lastTimeRewardApplicable() public view returns (uint256) {
+    return block.timestamp < periodFinish ? block.timestamp : periodFinish;
+  }
+
+  function rewardPerToken() public view returns (uint256) {
+    if (_totalSupply == 0) {
+      return rewardPerTokenStored;
+    }
+    return
+      rewardPerTokenStored.add(
+        lastTimeRewardApplicable()
+          .sub(lastUpdateTime)
+          .mul(rewardRate)
+          .mul(1e18)
+          .div(_totalSupply)
+      );
+  }
+
+  function earned(address account) public view returns (uint256) {
+    return
+      _balances[account]
+        .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+        .div(1e18)
+        .add(rewards[account]);
+  }
+
+  function getRewardForDuration() external view returns (uint256) {
+    return rewardRate.mul(rewardsDuration);
+  }
+
+  /* ========== ADMIN FUNCTIONS ========== */
+  function setPaused(bool _paused) external onlyOwner {
+    // Ensure we're actually changing the state before we do anything
+    if (_paused == paused()) {
+      return;
     }
 
-    /* ========== VIEWS ========== */
-
-    function totalSupply() external view returns (uint256) {
-        return _totalSupply;
+    if (_paused) {
+      _pause();
+      return;
     }
 
-    function balanceOf(address account) external view returns (uint256) {
-        return _balances[account];
+    _unpause();
+  }
+
+  function setRewardsDistribution(address _rewardsDistribution)
+    external
+    onlyOwner
+  {
+    rewardsDistribution = _rewardsDistribution;
+  }
+
+  /* ========== MUTATIVE FUNCTIONS ========== */
+
+  function stake(uint256 amount)
+    external
+    nonReentrant
+    whenNotPaused
+    updateReward(msg.sender)
+  {
+    if (amount <= 0) revert TokenVault_CannotStakeZeroAmount();
+
+    _totalSupply = _totalSupply.add(amount);
+    _balances[msg.sender] = _balances[msg.sender].add(amount);
+    stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+
+    emit Staked(msg.sender, amount);
+  }
+
+  function withdraw(uint256 amount)
+    public
+    nonReentrant
+    updateReward(msg.sender)
+  {
+    if (amount <= 0) revert TokenVault_CannotWithdrawZeroAmount();
+
+    _totalSupply = _totalSupply.sub(amount);
+    _balances[msg.sender] = _balances[msg.sender].sub(amount);
+    stakingToken.safeTransfer(msg.sender, amount);
+
+    emit Withdrawn(msg.sender, amount);
+  }
+
+  function getReward() public nonReentrant updateReward(msg.sender) {
+    uint256 reward = rewards[msg.sender];
+    if (reward > 0) {
+      rewards[msg.sender] = 0;
+      IERC20Upgradeable(rewardsToken).safeTransfer(msg.sender, reward);
+      emit RewardPaid(msg.sender, reward);
+    }
+  }
+
+  function exit() external {
+    withdraw(_balances[msg.sender]);
+    getReward();
+  }
+
+  /* ========== RESTRICTED FUNCTIONS ========== */
+
+  function notifyRewardAmount(uint256 reward)
+    external
+    onlyRewardsDistribution
+    updateReward(address(0))
+  {
+    if (block.timestamp >= periodFinish) {
+      rewardRate = reward.div(rewardsDuration);
+    } else {
+      uint256 remaining = periodFinish.sub(block.timestamp);
+      uint256 leftover = remaining.mul(rewardRate);
+      rewardRate = reward.add(leftover).div(rewardsDuration);
     }
 
-    function lastTimeRewardApplicable() public view returns (uint256) {
-        return block.timestamp < periodFinish ? block.timestamp : periodFinish;
+    // Ensure the provided reward amount is not more than the balance in the contract.
+    // This keeps the reward rate in the right range, preventing overflows due to
+    // very high values of rewardRate in the earned and rewardsPerToken functions;
+    // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
+    uint256 balance = IERC20Upgradeable(rewardsToken).balanceOf(address(this));
+    if (rewardRate > balance.div(rewardsDuration))
+      revert TokenVault_ProvidedRewardTooHigh();
+
+    lastUpdateTime = block.timestamp;
+    periodFinish = block.timestamp.add(rewardsDuration);
+    emit RewardAdded(reward);
+  }
+
+  // Added to support recovering LP Rewards from other systems such as BAL to be distributed to holders
+  function recoverERC20(address tokenAddress, uint256 tokenAmount)
+    external
+    onlyOwner
+  {
+    if (tokenAddress == address(stakingToken))
+      revert TokenVault_CannotWithdrawStakingToken();
+
+    IERC20Upgradeable(tokenAddress).safeTransfer(owner(), tokenAmount);
+
+    emit Recovered(tokenAddress, tokenAmount);
+  }
+
+  function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
+    if (block.timestamp <= periodFinish) {
+      revert TokenVault_RewardPeriodMustBeCompleted();
     }
 
+    rewardsDuration = _rewardsDuration;
 
-    function rewardPerToken() public view returns (uint256) {
-        if (_totalSupply == 0) {
-            return rewardPerTokenStored;
-        }
-        return
-            rewardPerTokenStored.add(
-                lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(_totalSupply)
-            );
+    emit RewardsDurationUpdated(rewardsDuration);
+  }
+
+  /* ========== MODIFIERS ========== */
+
+  modifier updateReward(address account) {
+    rewardPerTokenStored = rewardPerToken();
+    lastUpdateTime = lastTimeRewardApplicable();
+    if (account != address(0)) {
+      rewards[account] = earned(account);
+      userRewardPerTokenPaid[account] = rewardPerTokenStored;
     }
+    _;
+  }
 
-    function earned(address account) public view returns (uint256) {
-        return _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
-    }
-
-    function getRewardForDuration() external view returns (uint256) {
-        return rewardRate.mul(rewardsDuration);
-    }
-
-    /* ========== ADMIN FUNCTIONS ========== */
-    function setPaused(bool _paused) external onlyOwner {
-        // Ensure we're actually changing the state before we do anything
-        if (_paused == paused()) {
-            return;
-        }
-
-        if (_paused) {
-            _pause();
-            return;
-        } 
-
-        _unpause();
-    }
-
-    function setRewardsDistribution(address _rewardsDistribution) external onlyOwner {
-        rewardsDistribution = _rewardsDistribution;
-    }
-
-
-    /* ========== MUTATIVE FUNCTIONS ========== */
-
-    function stake(uint256 amount) external nonReentrant whenNotPaused updateReward(msg.sender) {
-        if (amount <= 0) revert TokenVault_CannotStakeZeroAmount();
-
-        _totalSupply = _totalSupply.add(amount);
-        _balances[msg.sender] = _balances[msg.sender].add(amount);
-        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
-
-        emit Staked(msg.sender, amount);
-    }
-
-    function withdraw(uint256 amount) public nonReentrant updateReward(msg.sender) {
-        if(amount <= 0) revert TokenVault_CannotWithdrawZeroAmount();
-        
-        _totalSupply = _totalSupply.sub(amount);
-        _balances[msg.sender] = _balances[msg.sender].sub(amount);
-        stakingToken.safeTransfer(msg.sender, amount);
-
-        emit Withdrawn(msg.sender, amount);
-    }
-
-    function getReward() public nonReentrant updateReward(msg.sender) {
-        uint256 reward = rewards[msg.sender];
-        if (reward > 0) {
-            rewards[msg.sender] = 0;
-            IERC20Upgradeable(rewardsToken).safeTransfer(msg.sender, reward);
-            emit RewardPaid(msg.sender, reward);
-        }
-    }
-
-    function exit() external {
-        withdraw(_balances[msg.sender]);
-        getReward();
-    }
-
-    /* ========== RESTRICTED FUNCTIONS ========== */
-
-    function notifyRewardAmount(uint256 reward)  external onlyRewardsDistribution updateReward(address(0)) {
-        if (block.timestamp >= periodFinish) {
-            rewardRate = reward.div(rewardsDuration);
-        } else {
-            uint256 remaining = periodFinish.sub(block.timestamp);
-            uint256 leftover = remaining.mul(rewardRate);
-            rewardRate = reward.add(leftover).div(rewardsDuration);
-        }
-
-        // Ensure the provided reward amount is not more than the balance in the contract.
-        // This keeps the reward rate in the right range, preventing overflows due to
-        // very high values of rewardRate in the earned and rewardsPerToken functions;
-        // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
-        uint balance = IERC20Upgradeable(rewardsToken).balanceOf(address(this));
-        if(rewardRate > balance.div(rewardsDuration)) revert TokenVault_ProvidedRewardTooHigh();
-
-        lastUpdateTime = block.timestamp;
-        periodFinish = block.timestamp.add(rewardsDuration);
-        emit RewardAdded(reward);
-    }
-
-    // Added to support recovering LP Rewards from other systems such as BAL to be distributed to holders
-    function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
-        if (tokenAddress == address(stakingToken)) revert TokenVault_CannotWithdrawStakingToken();
-
-        IERC20Upgradeable(tokenAddress).safeTransfer(owner(), tokenAmount);
-
-        emit Recovered(tokenAddress, tokenAmount);
-    }
-
-    function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
-        if(
-            block.timestamp <= periodFinish
-        ) {
-            revert TokenVault_RewardPeriodMustBeCompleted();
-        }
-
-        rewardsDuration = _rewardsDuration;
-
-        emit RewardsDurationUpdated(rewardsDuration);
-    }
-
-    /* ========== MODIFIERS ========== */
-
-    modifier updateReward(address account) {
-        rewardPerTokenStored = rewardPerToken();
-        lastUpdateTime = lastTimeRewardApplicable();
-        if (account != address(0)) {
-            rewards[account] = earned(account);
-            userRewardPerTokenPaid[account] = rewardPerTokenStored;
-        }
-        _;
-    }
-
-    modifier onlyRewardsDistribution() {
-        if(msg.sender != rewardsDistribution) revert TokenVault_NotRewardsDistributionContract();
-        _;
-    }
+  modifier onlyRewardsDistribution() {
+    if (msg.sender != rewardsDistribution)
+      revert TokenVault_NotRewardsDistributionContract();
+    _;
+  }
 }
--- contracts/v0.8.16/interfaces/ITokenVault.sol
+++ contracts/v0.8.16/interfaces/ITokenVault.sol
@@ -2,32 +2,31 @@
 pragma solidity 0.8.16;
 
 interface ITokenVault {
+  // Views
 
-    // Views
+  function balanceOf(address account) external view returns (uint256);
 
-    function balanceOf(address account) external view returns (uint256);
+  function earned(address account) external view returns (uint256);
 
-    function earned(address account) external view returns (uint256);
+  function getRewardForDuration() external view returns (uint256);
 
-    function getRewardForDuration() external view returns (uint256);
+  function lastTimeRewardApplicable() external view returns (uint256);
 
-    function lastTimeRewardApplicable() external view returns (uint256);
+  function rewardPerToken() external view returns (uint256);
 
-    function rewardPerToken() external view returns (uint256);
+  function rewardsDistribution() external view returns (address);
 
-    function rewardsDistribution() external view returns (address);
+  function rewardsToken() external view returns (address);
 
-    function rewardsToken() external view returns (address);
+  function totalSupply() external view returns (uint256);
 
-    function totalSupply() external view returns (uint256);
+  // Mutative
 
-    // Mutative
+  function exit() external;
 
-    function exit() external;
+  function getReward() external;
 
-    function getReward() external;
+  function stake(uint256 amount) external;
 
-    function stake(uint256 amount) external;
-
-    function withdraw(uint256 amount) external;
+  function withdraw(uint256 amount) external;
 }

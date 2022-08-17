886f8a5a1bfc52b0f8e112e744f5fe16acedef3a
KamenRiderPowaa <timothee.pratt@gmail.com>
2022-08-17 11:29:30 +0700

    wip: update tokenVault's functionalities

--- contracts/v0.8.16/TokenVault.sol
+++ contracts/v0.8.16/TokenVault.sol
@@ -8,9 +8,15 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "../../lib/solmate/src/utils/SafeTransferLib.sol";
+import "../../lib/solmate/src/utils/FixedPointMathLib.sol";
 import "./interfaces/ITokenVault.sol";
+import "./interfaces/IMigrator.sol";
+import "./interfaces/IFeeModel.sol";
 
 contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
+  using SafeTransferLib for address;
+  using FixedPointMathLib for uint256;
   using SafeMath for uint256;
   using SafeERC20 for IERC20;
 
@@ -21,8 +27,18 @@ contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
   uint256 public periodFinish = 0;
   uint256 public rewardRate = 0;
   uint256 public rewardsDuration = 7 days;
+
   uint256 public lastUpdateTime;
   uint256 public rewardPerTokenStored;
+
+  /* ========== STATE VARIABLES: Migration Options ========== */
+  bool public isMigrated;
+  uint256 public campaignStartBlock;
+  uint256 public campaignEndBlock;
+  uint256 public migrateChainId;
+  IFeeModel public feeModel;
+  IMigrator public migrator;
+  address public controller;
 
   mapping(address => uint256) public userRewardPerTokenPaid;
   mapping(address => uint256) public rewards;
@@ -37,6 +53,14 @@ contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
   event RewardPaid(address indexed user, uint256 reward);
   event RewardsDurationUpdated(uint256 newDuration);
   event Recovered(address token, uint256 amount);
+  event SetMigrationOption(
+    bool isMigrated,
+    uint256 campaignEndBlock,
+    IFeeModel feeModel,
+    IMigrator migrator
+  );
+  event Migrate(uint256 stakingTokenAmount, uint256 ethAmount);
+  event ClaimETH(address indexed user, uint256 ethAmount);
 
   /* ========== ERRORS ========== */
   error TokenVault_CannotStakeZeroAmount();
@@ -45,16 +69,63 @@ contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
   error TokenVault_CannotWithdrawStakingToken();
   error TokenVault_RewardPeriodMustBeCompleted();
   error TokenVault_NotRewardsDistributionContract();
+  error TokenVault_InvalidFee();
+  error TokenVault_AlreadyMigrated();
+  error TokenVault_NotYetMigrated();
+  error TokenVault_InvalidChainId();
+  error TokenVault_NotController();
 
   /* ========== CONSTRUCTOR ========== */
   constructor(
     address _rewardsDistribution,
     address _rewardsToken,
-    address _stakingToken
+    address _stakingToken,
+    address _controller
   ) {
     rewardsToken = _rewardsToken;
     stakingToken = IERC20(_stakingToken);
     rewardsDistribution = _rewardsDistribution;
+    controller = _controller;
+  }
+
+  /* ========== MODIFIERS ========== */
+
+  modifier updateReward(address account) {
+    rewardPerTokenStored = rewardPerToken();
+    lastUpdateTime = lastTimeRewardApplicable();
+    if (account != address(0)) {
+      rewards[account] = earned(account);
+      userRewardPerTokenPaid[account] = rewardPerTokenStored;
+    }
+    _;
+  }
+
+  modifier onlyRewardsDistribution() {
+    if (msg.sender != rewardsDistribution) {
+      revert TokenVault_NotRewardsDistributionContract();
+    }
+    _;
+  }
+
+  modifier onlyController() {
+    if (controller != msg.sender) {
+      revert TokenVault_NotController();
+    }
+    _;
+  }
+
+  modifier whenNotMigrated() {
+    if (isMigrated) {
+      revert TokenVault_AlreadyMigrated();
+    }
+    _;
+  }
+
+  modifier whenMigrated() {
+    if (!isMigrated) {
+      revert TokenVault_NotYetMigrated();
+    }
+    _;
   }
 
   /* ========== VIEWS ========== */
@@ -63,8 +134,8 @@ contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
     return _totalSupply;
   }
 
-  function balanceOf(address account) external view returns (uint256) {
-    return _balances[account];
+  function balanceOf(address _account) external view returns (uint256) {
+    return _balances[_account];
   }
 
   function lastTimeRewardApplicable() public view returns (uint256) {
@@ -85,15 +156,15 @@ contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
       );
   }
 
-  function earned(address account) public view returns (uint256) {
+  function earned(address _account) public view returns (uint256) {
     return
-      _balances[account]
-        .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+      _balances[_account]
+        .mul(rewardPerToken().sub(userRewardPerTokenPaid[_account]))
         .div(1e18)
-        .add(rewards[account]);
+        .add(rewards[_account]);
   }
 
-  function getRewardForDuration() external view returns (uint256) {
+  function GetRewardForDuration() external view returns (uint256) {
     return rewardRate.mul(rewardsDuration);
   }
 
@@ -119,38 +190,82 @@ contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
     rewardsDistribution = _rewardsDistribution;
   }
 
+  function setMigrationOption(
+    bool _isMigrated,
+    uint256 _campaignEndBlock,
+    IFeeModel _feeModel,
+    IMigrator _migrator
+  ) external onlyOwner {
+    isMigrated = _isMigrated;
+    campaignEndBlock = _campaignEndBlock;
+    feeModel = _feeModel;
+    migrator = _migrator;
+
+    emit SetMigrationOption(isMigrated, campaignEndBlock, feeModel, migrator);
+  }
+
   /* ========== MUTATIVE FUNCTIONS ========== */
 
-  function stake(uint256 amount)
+  function migrate() external onlyController nonReentrant whenNotMigrated {
+    if (block.chainid == 1) {
+      revert TokenVault_InvalidChainId();
+    }
+
+    isMigrated = true;
+
+    stakingToken.safeTransfer(address(migrator), _totalSupply);
+    migrator.execute();
+
+    uint256 feeWad = feeModel.getFee(
+      campaignStartBlock,
+      block.number,
+      campaignEndBlock
+    );
+    uint256 fee = feeWad.mulWadDown(address(this).balance);
+    msg.sender.safeTransferETH(fee);
+
+    emit Migrate(_totalSupply, fee);
+  }
+
+  function claimETH() external whenMigrated {
+    uint256 claimable = _balances[msg.sender].divWadDown(_totalSupply);
+    msg.sender.safeTransferETH(claimable);
+
+    emit ClaimETH(msg.sender, claimable);
+  }
+
+  function stake(uint256 _amount)
     external
     nonReentrant
     whenNotPaused
+    whenNotMigrated
     updateReward(msg.sender)
   {
-    if (amount <= 0) revert TokenVault_CannotStakeZeroAmount();
+    if (_amount <= 0) revert TokenVault_CannotStakeZeroAmount();
 
-    _totalSupply = _totalSupply.add(amount);
-    _balances[msg.sender] = _balances[msg.sender].add(amount);
-    stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+    _totalSupply = _totalSupply.add(_amount);
+    _balances[msg.sender] = _balances[msg.sender].add(_amount);
+    stakingToken.safeTransferFrom(msg.sender, address(this), _amount);
 
-    emit Staked(msg.sender, amount);
+    emit Staked(msg.sender, _amount);
   }
 
-  function withdraw(uint256 amount)
+  function withdraw(uint256 _amount)
     public
     nonReentrant
+    whenNotMigrated
     updateReward(msg.sender)
   {
-    if (amount <= 0) revert TokenVault_CannotWithdrawZeroAmount();
+    if (_amount <= 0) revert TokenVault_CannotWithdrawZeroAmount();
 
-    _totalSupply = _totalSupply.sub(amount);
-    _balances[msg.sender] = _balances[msg.sender].sub(amount);
-    stakingToken.safeTransfer(msg.sender, amount);
+    _totalSupply = _totalSupply.sub(_amount);
+    _balances[msg.sender] = _balances[msg.sender].sub(_amount);
+    stakingToken.safeTransfer(msg.sender, _amount);
 
-    emit Withdrawn(msg.sender, amount);
+    emit Withdrawn(msg.sender, _amount);
   }
 
-  function getReward() public nonReentrant updateReward(msg.sender) {
+  function claimGov() public nonReentrant updateReward(msg.sender) {
     uint256 reward = rewards[msg.sender];
     if (reward > 0) {
       rewards[msg.sender] = 0;
@@ -161,22 +276,22 @@ contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
 
   function exit() external {
     withdraw(_balances[msg.sender]);
-    getReward();
+    claimGov();
   }
 
   /* ========== RESTRICTED FUNCTIONS ========== */
 
-  function notifyRewardAmount(uint256 reward)
+  function notifyRewardAmount(uint256 _reward)
     external
     onlyRewardsDistribution
     updateReward(address(0))
   {
     if (block.timestamp >= periodFinish) {
-      rewardRate = reward.div(rewardsDuration);
+      rewardRate = _reward.div(rewardsDuration);
     } else {
       uint256 remaining = periodFinish.sub(block.timestamp);
       uint256 leftover = remaining.mul(rewardRate);
-      rewardRate = reward.add(leftover).div(rewardsDuration);
+      rewardRate = _reward.add(leftover).div(rewardsDuration);
     }
 
     // Ensure the provided reward amount is not more than the balance in the contract.
@@ -188,21 +303,22 @@ contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
       revert TokenVault_ProvidedRewardTooHigh();
 
     lastUpdateTime = block.timestamp;
+    campaignStartBlock = block.number;
     periodFinish = block.timestamp.add(rewardsDuration);
-    emit RewardAdded(reward);
+    emit RewardAdded(_reward);
   }
 
   // Added to support recovering LP Rewards from other systems such as BAL to be distributed to holders
-  function recoverERC20(address tokenAddress, uint256 tokenAmount)
+  function recoverERC20(address _tokenAddress, uint256 _tokenAmount)
     external
     onlyOwner
   {
-    if (tokenAddress == address(stakingToken))
+    if (_tokenAddress == address(stakingToken))
       revert TokenVault_CannotWithdrawStakingToken();
 
-    IERC20(tokenAddress).safeTransfer(owner(), tokenAmount);
+    IERC20(_tokenAddress).safeTransfer(owner(), _tokenAmount);
 
-    emit Recovered(tokenAddress, tokenAmount);
+    emit Recovered(_tokenAddress, _tokenAmount);
   }
 
   function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
@@ -213,23 +329,5 @@ contract TokenVault is ITokenVault, ReentrancyGuard, Pausable, Ownable {
     rewardsDuration = _rewardsDuration;
 
     emit RewardsDurationUpdated(rewardsDuration);
-  }
-
-  /* ========== MODIFIERS ========== */
-
-  modifier updateReward(address account) {
-    rewardPerTokenStored = rewardPerToken();
-    lastUpdateTime = lastTimeRewardApplicable();
-    if (account != address(0)) {
-      rewards[account] = earned(account);
-      userRewardPerTokenPaid[account] = rewardPerTokenStored;
-    }
-    _;
-  }
-
-  modifier onlyRewardsDistribution() {
-    if (msg.sender != rewardsDistribution)
-      revert TokenVault_NotRewardsDistributionContract();
-    _;
   }
 }

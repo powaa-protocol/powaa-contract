72e7b982aa7081f207a161a25eaa843e90ce7a65
HulkPowaa <hulk.powaa@gmail.com>
2022-08-18 13:10:32 +0700

    change : fix path

--- test/foundry/unit/controller/ControllerBase.t.sol
+++ test/foundry/unit/controller/ControllerBase.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.16;
 
 import "../_base/BaseTest.sol";
 import "mock-contract/MockContract.sol";
-import "../../../../../contracts/v0.8.16/Controller.sol";
+import "../../../../contracts/v0.8.16/Controller.sol";
 
 abstract contract ControllerBaseTest is BaseTest {
   MockContract internal mockTokenVault;

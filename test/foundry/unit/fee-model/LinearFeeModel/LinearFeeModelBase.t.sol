dcb4c1c0251615b9541a3f27a75e4f0e46c3b62b
HulkPowaa <hulk.powaa@gmail.com>
2022-08-18 17:28:38 +0700

    change : move folder fee linear uint test

--- test/foundry/unit/fee-model/LinearFeeModel/LinearFeeModelBase.t.sol
+++ test/foundry/unit/fee-model/LinearFeeModel/LinearFeeModelBase.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import "../_base/BaseTest.sol";
+import "../../_base/BaseTest.sol";
 
-import "../../../../contracts/v0.8.16/fee-model/LinearFeeModel.sol";
+import "../../../../../contracts/v0.8.16/fee-model/LinearFeeModel.sol";
 
 /// @title An abstraction of the LinearFeeModel Testing contract, containing a scaffolding method for creating the fixture
 abstract contract LinearFeeModelBaseTest is BaseTest {
@@ -14,13 +14,13 @@ abstract contract LinearFeeModelBaseTest is BaseTest {
     linearFeeModel = _setupLinearFeeModel(0, 100);
   }
 
-  function _setupLinearFeeModel(uint256 baseRate, uint256 multiplierRate)
+  function _setupLinearFeeModel(uint256 _baseRate, uint256 _multiplierRate)
     internal
     returns (LinearFeeModel)
   {
     LinearFeeModel _linearFeeModel = new LinearFeeModel(
-      baseRate,
-      multiplierRate
+      _baseRate,
+      _multiplierRate
     );
 
     return _linearFeeModel;

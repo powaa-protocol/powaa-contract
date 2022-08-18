04b836c4b4eea4ca8e6af3f6a0ac1a7816016257
KamenRiderPowaa <timothee.pratt@gmail.com>
2022-08-18 13:46:37 +0700

    test: separate test files per unit

--- test/foundry/unit/controller/Controller_TestMigration.t.sol
+++ test/foundry/unit/controller/Controller_TestMigration.t.sol
@@ -35,11 +35,4 @@ contract Controller_TestMigration is ControllerBaseTest {
     controller.migrate();
     vm.stopPrank();
   }
-
-  function test_WhenWhitelistVault() external {
-    MockContract mockTokenVault2 = new MockContract();
-    vm.expectEmit(true, true, true, true);
-    emit SetVault(address(mockTokenVault2));
-    controller.whitelistVault(address(mockTokenVault2));
-  }
 }
---
+++ test/foundry/unit/controller/Controller_TestWhitelistVault.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "./ControllerBase.t.sol";
+
+contract Controller_TestMigration is ControllerBaseTest {
+  // Controller event
+  event SetVault(address vault);
+
+  /// @dev foundry's setUp method
+  function setUp() public override {
+    super.setUp();
+  }
+
+  function test_WhenWhitelistVault() external {
+    MockContract mockTokenVault2 = new MockContract();
+    vm.expectEmit(true, true, true, true);
+    emit SetVault(address(mockTokenVault2));
+    controller.whitelistVault(address(mockTokenVault2));
+  }
+}

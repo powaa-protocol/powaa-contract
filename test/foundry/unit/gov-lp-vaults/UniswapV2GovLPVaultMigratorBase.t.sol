3ef20e81c8a1cf69e15ce62fde5ce5b3de087adf
HulkPowaa <hulk.powaa@gmail.com>
2022-08-23 18:22:31 +0700

    change : change structure

--- test/foundry/unit/gov-lp-vaults/UniswapV2GovLPVaultMigratorBase.t.sol
+++ test/foundry/unit/gov-lp-vaults/UniswapV2GovLPVaultMigratorBase.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.16;
 
 import "../_base/BaseTest.sol";
 import "../_mock/MockERC20.sol";
-import "../_mock/MockLpToken.sol";
+import "../_mock/MockGovLpToken.sol";
 import "../_mock/MockUniswapV2Router01.sol";
 import "mock-contract/MockContract.sol";
 import "../../../../contracts/v0.8.16/interfaces/apis/IUniswapV2Router02.sol";
@@ -15,35 +15,28 @@ abstract contract UniswapV2GovLPVaultMigratorBaseTest is BaseTest {
   using SafeTransferLib for address;
 
   MockUniswapV2Router01 internal mockRouter;
-  MockLpToken internal mockLp;
+  MockGovLpToken internal mockLp;
   MockERC20 internal mockToken;
 
   UniswapV2GovLPVaultMigrator internal uniswapV2GovLPVaultMigrator;
 
-  address public constant WETH9 =
-    address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-
   /// @dev Foundry's setUp method
   function setUp() public virtual {
     mockRouter = new MockUniswapV2Router01();
-    mockLp = _setupMockLpToken("RAND-MOCK-GLP", "RMGLP");
+
     mockToken = _setupFakeERC20("MockToken", "MT");
+    mockLp = _setupMockLPToken("Gov LP Token", "G-LP");
 
     uniswapV2GovLPVaultMigrator = _setupUniswapV2GovLPVaultMigrator(
       IUniswapV2Router02(address(mockRouter))
     );
-    mockLp.givenMethodReturnAddress(
-      abi.encodePacked(bytes4(keccak256("token0()"))),
-      address(mockToken)
-    );
-    mockLp.givenMethodReturnAddress(
-      abi.encodePacked(bytes4(keccak256("token1()"))),
-      address(WETH9)
-    );
 
     // pre-minted token for mocking purposes
     mockLp.mint(address(uniswapV2GovLPVaultMigrator), 1e18);
-    mockToken.mint(address(uniswapV2GovLPVaultMigrator), 1e18);
+    MockERC20(payable(address(mockLp.token0()))).mint(
+      address(uniswapV2GovLPVaultMigrator),
+      1e18
+    );
     vm.deal(address(mockRouter), 1e18);
   }
 
@@ -75,21 +68,14 @@ abstract contract UniswapV2GovLPVaultMigratorBaseTest is BaseTest {
     return MockERC20(payable(_proxy));
   }
 
-  function _setupMockLpToken(string memory _name, string memory _symbol)
+  function _setupMockLPToken(string memory _name, string memory _symbol)
     internal
-    returns (MockLpToken)
+    returns (MockGovLpToken)
   {
-    MockLpToken _impl = new MockLpToken();
-    TransparentUpgradeableProxy _proxy = new TransparentUpgradeableProxy(
-      address(_impl),
-      address(proxyAdmin),
-      abi.encodeWithSelector(
-        bytes4(keccak256("initialize(string,string)")),
-        _name,
-        _symbol
-      )
-    );
-    return MockLpToken(payable(_proxy));
+    MockERC20 mockRewardToken = _setupFakeERC20("MockRewardToken", "MRT");
+    MockGovLpToken _impl = new MockGovLpToken(IERC20(address(mockRewardToken)));
+    _impl.initialize(_name, _symbol);
+    return MockGovLpToken(payable(_impl));
   }
 
   /// @dev Fallback function to accept ETH.

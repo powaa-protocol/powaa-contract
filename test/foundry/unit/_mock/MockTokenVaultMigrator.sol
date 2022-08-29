0e0769b355ed84573e8107609968b9f575559d97
KamenRiderPowaa <timothee.pratt@gmail.com>
2022-08-29 11:38:26 +0700

    test: separate mockTokenVaultMigrator and mockGovLPMigrator since both contain completely diffferent logic not

--- test/foundry/unit/_mock/MockTokenVaultMigrator.sol
+++ test/foundry/unit/_mock/MockTokenVaultMigrator.sol
@@ -7,30 +7,18 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../../../../lib/solmate/src/utils/SafeTransferLib.sol";
 import "../../../../lib/mock-contract/contracts/MockContract.sol";
 
-contract MockMigrator is MockContract {
+contract MockTokenVaultMigrator is MockContract {
   using SafeTransferLib for address;
   using SafeMath for uint256;
 
-  bool private supportGovLpVault = false;
   mapping(address => uint256) private exchangeRate;
-
-  constructor(bool _supportGovLpVault) {
-    supportGovLpVault = _supportGovLpVault;
-  }
 
   function mockSetMigrateRate(address _token, uint256 _exchangeRate) external {
     exchangeRate[_token] = _exchangeRate;
   }
 
   function execute(bytes calldata _data) external {
-    address token;
-    if (supportGovLpVault) {
-      token = abi.decode(_data, (address));
-    } else {
-      uint24 poolFee;
-      (token, poolFee) = abi.decode(_data, (address, uint24));
-      poolFee;
-    }
+    (address token, ) = abi.decode(_data, (address, uint24));
 
     uint256 tokenBalance = IERC20(token).balanceOf(address(this));
     uint256 migratedAmount = tokenBalance.mul(exchangeRate[token]).div(1 ether);
